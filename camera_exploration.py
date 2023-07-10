7919070a271e21a54a393f6dd98a7ad3b47f75d9
Andy Singh <andys.dstest@gmail.com>
2023-07-09 21:17:19 -0700

    Added calculating MSE to see if that can be used

--- camera_exploration.py
+++ camera_exploration.py
@@ -52,13 +52,31 @@ while True:
     index_to_update = frames_captured % n
     previous_frames[:, :, index_to_update] = gray
 
+    # Take action if certain percentage of pixels are 0
     if np.count_nonzero(difference == 0) > motion_threshold * np.size(difference):
         print("Motion Detected")
+
+    # Calculate MSE (could be another metric to use)
+    # mse = np.mean(abs(previous_frames[:, :, (frames_captured-1) % n]-gray))
+    mse = np.mean(abs(median_image - gray))
+    print(mse)
 
     # Display the resulting frame
     cv.imshow('frame', frame)
     cv.imshow('median', median_image.astype(np.uint8))
     cv.imshow('threshold', difference)
+
+    # I wasn't able to get these to return a result for the sample video even with tuning the parameters
+    # backSub = cv.createBackgroundSubtractorMOG2(history=500, varThreshold=16, detectShadows=True)
+    # backSub = cv.createBackgroundSubtractorKNN(history=10, dist2Threshold=1000, detectShadows=False)
+    # fgMask = backSub.apply(gray, learningRate=0.1)
+
+    # cv.rectangle(frame, (10, 2), (100, 20), (255, 255, 255), -1)
+    # cv.putText(frame, str(cap.get(cv.CAP_PROP_POS_FRAMES)), (15, 15),
+    #            cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
+    #
+    # cv.imshow('Frame', gray)
+    # cv.imshow('FG Mask', fgMask)
 
     if cv.waitKey(1) & 0xFF == ord('q'):
         break
