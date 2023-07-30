31c1204ce8b786ef16fd6322f216577fade789aa
Andy Singh <andys.dstest@gmail.com>
2023-07-29 18:13:22 -0700

    Additional correction for local write path

--- image_write.py
+++ image_write.py
@@ -8,6 +8,7 @@ from datetime import datetime
 import camera_logging
 
 LOCAL_OUTPUT_FOLDER = 'local_output'  # Local folder for where to output images
+LOCAL_OUTPUT_SUBFOLDER = datetime.now().strftime('%Y%m%d')  # e.g. 20230729
 
 
 # Connect to Azure using environment variables loaded the .env file
@@ -40,26 +41,24 @@ def write_image_to_azure(azure_container_client, picam2_obj, filename):
 # Method for handling writing image locally
 def write_image_locally(picam2_obj, filename, difference_img, main_img, lores_img, capture_again, debug):
     if capture_again:
-        picam2_obj.capture_file(os.path.join(LOCAL_OUTPUT_FOLDER, filename + '.jpg'))
+        picam2_obj.capture_file(os.path.join(LOCAL_OUTPUT_FOLDER, LOCAL_OUTPUT_SUBFOLDER, filename + '.jpg'))
     else:
-        cv2.imwrite(os.path.join(LOCAL_OUTPUT_FOLDER, filename + '.jpg'), main_img)
+        cv2.imwrite(os.path.join(LOCAL_OUTPUT_FOLDER, LOCAL_OUTPUT_SUBFOLDER, filename + '.jpg'), main_img)
 
     if debug:
         difference_normalize = cv2.normalize(difference_img, dst=None,
                                              alpha=0, beta=255, norm_type=cv2.NORM_MINMAX)
-        cv2.imwrite(os.path.join(LOCAL_OUTPUT_FOLDER, "difference", filename + '.png'),
+        cv2.imwrite(os.path.join(LOCAL_OUTPUT_FOLDER, LOCAL_OUTPUT_SUBFOLDER, "difference", filename + '.png'),
                     difference_normalize)
-        cv2.imwrite(os.path.join(LOCAL_OUTPUT_FOLDER, "lores", filename + '.png'), lores_img)
+        cv2.imwrite(os.path.join(LOCAL_OUTPUT_FOLDER, LOCAL_OUTPUT_SUBFOLDER, "lores", filename + '.png'), lores_img)
     camera_logging.output_log(camera_logging.EVENT_IMAGE_WRITTEN_LOCAL, ["Filename: " + filename])
 
 
 # Method for creating folder directories for local image write if they don't
 def create_local_write_folder():
-    folder_date = datetime.now()
-    folder_name = folder_date.strftime('%Y%m%d')
-    difference_path = os.path.join(LOCAL_OUTPUT_FOLDER, folder_name, "difference")
-    lores_path = os.path.join(LOCAL_OUTPUT_FOLDER, folder_name, "lores")
+    difference_path = os.path.join(LOCAL_OUTPUT_FOLDER, LOCAL_OUTPUT_SUBFOLDER, "difference")
+    lores_path = os.path.join(LOCAL_OUTPUT_FOLDER, LOCAL_OUTPUT_SUBFOLDER, "lores")
     if not os.path.exists(difference_path):
-        os.makedirs(os.path.join(LOCAL_OUTPUT_FOLDER, folder_name, "difference"), True)
+        os.makedirs(difference_path, True)
     if not os.path.exists(lores_path):
-        os.makedirs(os.path.join(LOCAL_OUTPUT_FOLDER, folder_name, "lores"), True)
+        os.makedirs(lores_path, True)
